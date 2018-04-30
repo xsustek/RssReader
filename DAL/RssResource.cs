d377b1bdac71b40c3185f44fc72992adad90713c
Milan Šůstek <milan.sustek@gmail.com>
2018-04-30 14:50:39 +0200

    realm dal

---
+++ DAL/RssResource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Realms;
+
+namespace DAL
+{
+    public class RssResource : RealmObject
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
